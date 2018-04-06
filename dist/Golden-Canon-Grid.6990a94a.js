53f462994db6774187c36b4dbb0c303e6908a7e5
MMBA\tdutra <thiago.dutra@mediamonks.com>
2018-04-06 20:49:03 -0300

    area 1 text position

--- dist/Golden-Canon-Grid.6990a94a.js
+++ dist/Golden-Canon-Grid.6990a94a.js
@@ -144,7 +144,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":10}],25:[function(require,module,exports) {
+},{"_css_loader":10}],26:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -313,5 +313,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[25])
+},{}]},{},[26])
 //# sourceMappingURL=/Golden-Canon-Grid.6990a94a.map
